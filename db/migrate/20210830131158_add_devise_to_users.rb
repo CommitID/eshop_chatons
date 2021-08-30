0eba6d398fe251add7636af8f74775a0c916f654
rthpm <rthp@fastmail.com>
2021-08-31 00:23:31 +0200

    Some small fix on the table organization

--- db/migrate/20210830131158_add_devise_to_users.rb
+++ db/migrate/20210830131158_add_devise_to_users.rb
@@ -3,6 +3,9 @@
 class AddDeviseToUsers < ActiveRecord::Migration[5.2]
   def self.up
     change_table :users do |t|
+      t.string :first_name,         null: false
+      t.string :last_name,          null: false
+
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -39,7 +42,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.2]
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
+    add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
 
