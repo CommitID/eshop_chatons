dd262dc27b277e2aa44126f9f8d38b9e2444a4d2
rthpm <rthp@fastmail.com>
2021-08-31 16:18:15 +0200

    better routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
-  resources :orders
+  root 'items#index'
   devise_for :users
-  root to: "items#index"
 
-  resources :carts
-  resources :users, only: [:show, :edit, :update]
+  get 'carts/:id' => 'carts#show', as: 'cart'
+  delete 'carts/:id' => 'carts#destroy'
+
+  post 'cart_items/:id/add' => 'cart_items#add_quantity', as: 'cart_item_add'
+  post 'cart_items/:id/reduce' => 'cart_items#reduce_quantity', as: 'cart_item_reduce'
+  post 'cart_items' => 'cart_items#create'
+  get 'cart_items/:id' => 'cart_items#show', as: 'cart_item'
+  delete 'cart_items/:id' => 'cart_items#destroy'
+
+  resources :users, only: %i[show edit update], path: 'profil'
   resources :items
+  resources :orders
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
