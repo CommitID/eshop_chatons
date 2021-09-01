1a06ae092573024c40a41c5c830fce4355656dc4
rthpm <rthp@fastmail.com>
2021-09-01 22:07:44 +0200

    Quickfix on the the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,5 @@
 20.times do
   Item.create(title: Faker::Creature::Cat.name,
               description: Faker::Lorem.sentence,
-              price: rand(100..500),
-              picture: Faker::Lorem.word)
+              price: rand(100..500))
 end
