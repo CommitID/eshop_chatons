147a82bbeaadbccb3553396cdab3268c3d8684d4
rthpm <rthp@fastmail.com>
2021-09-01 14:49:34 +0200

    Seed flexible, show uploaded images & fallback

--- Gemfile
+++ Gemfile
@@ -39,7 +39,6 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'autoprefixer-rails'
 gem 'aws-sdk-s3', '~> 1'
 gem 'devise'
-gem 'dotenv-rails', groups: %i[development test]
 gem 'font-awesome-rails'
 gem 'foundation-rails'
 gem 'jquery-rails'
@@ -48,6 +47,7 @@ gem 'stripe'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
+  gem 'dotenv-rails'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-20.times do 
-    Item.create(title: Faker::Creature::Cat.name, 
-                description: Faker::Lorem.sentence,
-                price: rand(100..500),
-                image_url: Faker::Lorem.word
-                )
+20.times do
+  Item.create(title: Faker::Creature::Cat.name,
+              description: Faker::Lorem.sentence,
+              price: rand(100..500),
+              picture: Faker::Lorem.word)
 end
