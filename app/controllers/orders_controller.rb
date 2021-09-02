cdd8399a617d9ac922de10f27cf7b7dab53931ed
rthpm <rthp@fastmail.com>
2021-09-02 12:47:14 +0200

    Fixed the OrderItem relation to Orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,31 +1,21 @@
 class OrdersController < ApplicationController
-  before_action :set_order, only: %i[show edit update destroy]
   before_action :authenticate_user!
 
-  # GET /orders or /orders.json
   def index
     @orders = Order.all
   end
 
-  # GET /orders/1 or /orders/1.json
-  def show; end
-
-  # GET /orders/new
-  def new
-    @order = Order.new
+  def show
+    @order = Order.find(params[:id])
   end
 
-  # GET /orders/1/edit
-  def edit; end
-
-  # POST /orders or /orders.json
   def create
     @cart = @cart
     @total = @cart.total_price
 
     @order = Order.new(user: current_user)
     @order_items = @cart.items.each do |it|
-      OrderItem.create(item_id: it.id, order_id: @order.id)
+      OrderItem.create(item_id: it.id, order: @order)
     end
 
     if @order.save
@@ -36,17 +26,5 @@ class OrdersController < ApplicationController
       flash[:alert] = 'Pas bravo'
       redirect_to @cart
     end
-  end
-
-  private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_order
-    @order = Order.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
-  def order_params
-    params.fetch(:order, {})
   end
 end
