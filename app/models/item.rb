b1697525a6837dbbcc831f77dda7eee9c6399c28
rthpm <rthp@fastmail.com>
2021-09-02 14:31:34 +0200

    Safety measure in pdocution mode

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,15 @@ class Item < ApplicationRecord
   validates :title, presence: true
   # validates :description, presence: true
   validates :price, presence: true
-  # validates :image_url, presence: true
+  validates :title, uniqueness: true if Rails.env.production?
   has_many :cart_items
   has_many :cart
   has_many :order_items
   has_many :order
   has_one_attached :picture
+  validate :picture_validation
+
+  def picture_validation
+    errors[:base] << 'Veuillez associer une image au produit' unless picture.attached?
+  end
 end
