0b2787428ff1031f0bd5af629f8559c7fcae2456
rthpm <rthp@fastmail.com>
2021-09-02 12:47:34 +0200

    Standardized the order#show view

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,12 @@ class Order < ApplicationRecord
   has_many :order_items
   has_many :items, through: :order_items
   belongs_to :user
+
+  def total_price
+    price = 0
+    order_items.each do |i|
+      price += i.item.price
+    end
+    price
+  end
 end
